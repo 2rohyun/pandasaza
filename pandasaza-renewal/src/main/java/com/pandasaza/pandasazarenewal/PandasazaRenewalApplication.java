a908e42f7d4b166f0d25d9a4b0025a43c7f93cf3
2rohyun <dlehgus5656@naver.com>
2021-05-27 16:42:52 +0900

    main: Renewal pandasaaza API server
    ADD project settings for renewal pandasaza API server
    ADD setting for gitignore

---
+++ pandasaza-renewal/src/main/java/com/pandasaza/pandasazarenewal/PandasazaRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.pandasaza.pandasazarenewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandasazaRenewalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandasazaRenewalApplication.class, args);
+    }
+
+}
---
+++ pandasaza-renewal/src/test/java/com/pandasaza/pandasazarenewal/PandasazaRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandasaza.pandasazarenewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandasazaRenewalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
